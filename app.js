7eea0c2312118f7dfe8a7be1be7db303b60be400
NLessW <qkqkhih55@gmail.com>
2025-01-22 18:56:43 +0900

    line practice

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
+// Practice Rectangle
 /*
 ctx.rect(50, 50, 100, 100);
 ctx.rect(150, 150, 100, 100);
@@ -15,4 +16,14 @@ ctx.fillStyle = "red";
 setTimeout(() => {
   ctx.fill();
 }, 5000);
+*/
+
+// Practice Draw Line
+/*
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
+ctx.stroke();
 */
